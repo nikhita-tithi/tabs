56b7b7440821fc3418d3541056c9c75dd1f39694
Nikhita Tithi <nikhita.tithi@gmail.com>
2020-08-21 20:29:23 +0530

    Updates

---
+++ precache-manifest.da8399c90d636327573294683545f62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1925929a8ea34e9e19609e2287dc936f",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "410bc578a4546695cdab",
+    "url": "/tabs/static/css/main.c1bb45bd.chunk.css"
+  },
+  {
+    "revision": "9c3ff37c270d7e75b47f",
+    "url": "/tabs/static/js/2.1db6efb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs/static/js/2.1db6efb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410bc578a4546695cdab",
+    "url": "/tabs/static/js/main.485c15c3.chunk.js"
+  },
+  {
+    "revision": "fc2daf50102a6651cc3f",
+    "url": "/tabs/static/js/runtime-main.50b1e45c.js"
+  }
+]);
